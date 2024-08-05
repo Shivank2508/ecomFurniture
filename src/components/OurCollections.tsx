073feca72579528c2f885ec29e1284ebae82cc3d
shivank tyagi <tyagishivank1212@gmail.com>
2024-08-05 21:13:25 +0530

    best

--- src/components/OurCollections.tsx
+++ src/components/OurCollections.tsx
@@ -2,58 +2,59 @@
 
 export const OurCollections = () => {
     return (
-        <div className="p-4 ">
-            <h1 className="text-center pb-4 pt-4 fs-1 fw-bolder"> Our Collections</h1>
-            <div className="d-flex align-content-start  justify-content-center flex-wrap  mt-4" >
-                <a href="sofa-sets " className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none" >
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon1.jpg" data-src="	https://www.woodenstreet.com/images/home-new1/icon1.jpg" alt="Wooden Sofa Set Furniture" title="Sofa set online" width="100%" height="100%" data-was-processed="true" />
-                    <p className=" fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-2" >Sofa Sets</p>
-                </a>
-                <a href="beds" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon2.jpg" data-src="images/home-new1/icon2.jpg" alt="Wooden Bed Furniture" title="Wooden Bed" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">beds</p>
-                </a>
-                <a href="dining-table-sets" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon3.jpg" data-src="images/home-new1/icon3.jpg" alt="Wooden Dining Furniture" title="Dining Table set" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Dining Table Sets</p>
-                </a>
-                <a href="sofa-cum-beds" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon4.jpg" data-src="images/home-new1/icon4.jpg" alt="Wooden Sofa Come Bed Furniture" title="Sofa cum bed online" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Sofa Cum Beds</p>
-                </a>
-                <a href="tv-units" className=" d-flex justify-content-center align-items-center flex-column text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon10.jpg" data-src="images/home-new1/icon10.jpg" alt="Wooden Tv Unit Furniture" title="Wooden TV Unit" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">TV Units</p>
-                </a>
-                <a href="bookshelves" className=" d-flex justify-content-center align-items-center flex-column text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon7.jpg" data-src="images/home-new1/icon7.jpg" alt="Wooden Bookshelf Furniture Online" title="Book Shelves online" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">book shelves</p>
-                </a>
-                <a href="coffee-tables" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon8.jpg" data-src="images/home-new1/icon8.jpg" alt="Buy Coffee Table for Home | Wooden Coffee Table Furniture" title="Coffee Table Sets online" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">coffee Tables</p>
-                </a>
-                <a href="study-tables" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon6.jpg" data-src="images/home-new1/icon6.jpg" alt="Wooden Study Table Furniture" title="Study Table" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Study Tables</p>
-                </a>
-                <a href="home-decors" className=" d-flex justify-content-center align-items-center flex-column text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon11.jpg" data-src="images/home-new1/icon11.jpg" alt="Home Decor Items " title="Decor Item" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Home Decor</p>
-                </a>
-                <a href="home-furnishing" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon9.jpg" data-src="images/home-new1/icon9.jpg" alt="Home Furnishing Items " title="Home Furnishing Online" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Home Furnishing</p>
-                </a>
-                <a href="lamps-and-lighting" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon12.jpg" data-src="images/home-new1/icon12.jpg" alt="Lamp Lights Online in India" title="Lamp and Lighting" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Lamps &amp; Lightings</p>
-                </a>
-                <a href="furniture-sale" className=" d-flex justify-content-center align-items-center flex-column  text-black text-decoration-none">
-                    <img style={{ width: "70%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon5.jpg" data-src="images/home-new1/icon5.jpg" alt="Furniture Sale Online in India" title="Wooden Online Sale" width="100%" height="100%" data-was-processed="true" />
-                    <p className="fw-bolder fs-6 pe-4 text-bleck text-decoration-none mt-4">Sale</p>
-                </a>
-            </div>
-        </div >
+        <div className="container">
+        <h1 className="text-center pb-4 pt-4 fs-1 fw-bolder">Our Collections</h1>
+        <div className="d-flex flex-wrap justify-content-between mt-4">
+          <a href="sofa-sets" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none " style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon1.jpg" alt="Wooden Sofa Set Furniture" title="Sofa set online" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-2">Sofa Sets</p>
+          </a>
+          <a href="beds" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon2.jpg" alt="Wooden Bed Furniture" title="Wooden Bed" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Beds</p>
+          </a>
+          <a href="dining-table-sets" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon3.jpg" alt="Wooden Dining Furniture" title="Dining Table set" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Dining Table Sets</p>
+          </a>
+          <a href="sofa-cum-beds" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon4.jpg" alt="Wooden Sofa Come Bed Furniture" title="Sofa cum bed online" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Sofa Cum Beds</p>
+          </a>
+          <a href="tv-units" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon10.jpg" alt="Wooden Tv Unit Furniture" title="Wooden TV Unit" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">TV Units</p>
+          </a>
+          <a href="bookshelves" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon7.jpg" alt="Wooden Bookshelf Furniture Online" title="Book Shelves online" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Book Shelves</p>
+          </a>
+          <a href="coffee-tables" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon8.jpg" alt="Buy Coffee Table for Home | Wooden Coffee Table Furniture" title="Coffee Table Sets online" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Coffee Tables</p>
+          </a>
+          <a href="study-tables" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon6.jpg" alt="Wooden Study Table Furniture" title="Study Table" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Study Tables</p>
+          </a>
+          <a href="home-decors" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon11.jpg" alt="Home Decor Items" title="Decor Item" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Home Decor</p>
+          </a>
+          <a href="home-furnishing" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon9.jpg" alt="Home Furnishing Items" title="Home Furnishing Online" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Home Furnishing</p>
+          </a>
+          <a href="lamps-and-lighting" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon12.jpg" alt="Lamp Lights Online in India" title="Lamp and Lighting" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Lamps & Lightings</p>
+          </a>
+          <a href="furniture-sale" className="d-flex flex-column justify-content-center align-items-center text-black text-decoration-none p-3 m-2" style={{ flex: "0 0 23%" }}>
+            <img style={{ width: "85%", height: "auto" }} src="https://www.woodenstreet.com/images/home-new1/icon5.jpg" alt="Furniture Sale Online in India" title="Wooden Online Sale" />
+            <p className="fw-bolder fs-6 pe-4 text-black text-decoration-none mt-4">Sale</p>
+          </a>
+        </div>
+      </div>
+      
     )
 }
